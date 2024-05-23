b9455540e4da2f27ff8bbe8c832f716d6002f6b9
sohyun2023 <132315029+sohyun2023@users.noreply.github.com>
2024-05-23 17:09:42 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,14 @@
+def solution(n,a,b):
+    round=0
+    while True:
+        round=round+1
+        a=(a+1)//2
+        b=(b+1)//2
+        
+        if a ==b:
+            break
+        
+    
+    
+    
+    return round
