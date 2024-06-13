2c97ff03b3a3691c1c6eeb6741dedb2afb8f6db7
sohyun2023 <132315029+sohyun2023@users.noreply.github.com>
2024-06-13 11:09:02 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer=n+1
+    list=[]
+    while True:
+        
+        if bin(answer).count('1')==bin(n).count('1'):
+                return answer
+        answer=answer+1
+        
+    
+    
+            
+            
