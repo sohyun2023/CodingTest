81f2de6b35c99030ef8150becbb6d56ebd2b4d06
sohyun2023 <132315029+sohyun2023@users.noreply.github.com>
2024-06-20 21:14:19 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,27 @@
+def solution(want, number, discount):
+    from collections import Counter
+
+    want_dict = dict(zip(want, number))
+    want_length = len(discount)
+
+    def matches(want_dict, current_window):
+        for item in want_dict:
+            if want_dict[item] != current_window.get(item, 0):
+                return False
+        return True
+
+    current_window = Counter(discount[:10])
+    valid_days = 0
+
+    if matches(want_dict, current_window):
+        valid_days += 1
+
+    for i in range(10, want_length):
+        current_window[discount[i - 10]] -= 1
+        if current_window[discount[i - 10]] == 0:
+            del current_window[discount[i - 10]]
+        current_window[discount[i]] += 1
+        if matches(want_dict, current_window):
+            valid_days += 1
+
+    return valid_days
