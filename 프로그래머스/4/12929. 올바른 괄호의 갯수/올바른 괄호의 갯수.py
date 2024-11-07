6681670176d7c3916f77da370f73fad315e2cef1
sohyun2023 <132315029+sohyun2023@users.noreply.github.com>
2024-11-07 20:25:48 +0900

    Update 올바른 괄호의 갯수.py

--- 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.py
@@ -3,7 +3,7 @@ def solution(k):
         return 1
     catalan = [0] * (k + 1)
     catalan[0] = 1
-    for i in range(1, k + 1):
+    for i in range(1, k + 1): #
         for j in range(i):
             catalan[i] += catalan[j] * catalan[i - 1 - j]
     return catalan[k]
